13cd83a9ecb7d96671b8b0d914e29bc6b96fc15f
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-10-09 15:24:04 +0300

    Tests

--- src/test/java/com/github/nikita_volkov/java/transducers/Test.java
+++ src/test/java/com/github/nikita_volkov/java/transducers/Test.java
@@ -6,12 +6,24 @@ import junit.framework.TestCase;
 import java.util.Arrays;
 
 public class Test extends TestCase {
-  public void test1() {
-    Transducer<Integer, String> transducer = new ComposingTransducer<>(new UniquifyingTransducer<>(), new MappingTransducer<>(a -> a.toString()));
-    assertEquals("1234", transducer.transduce(new CatIteration()).consume(Arrays.asList(1, 2, 3, 4)));
+  public void testUniquifyingTransducer() {
+    Transducer<String, String> transducer = new UniquifyingTransducer<>();
+    assertEquals("123", transducer.transduce(new CatIteration()).consume(Arrays.asList("1", "2", "2", "3")));
   }
-  public void test2() {
-    Transducer<Integer, String> transducer = new ComposingTransducer<>(new UniquifyingTransducer<>(), new MappingTransducer<>(a -> a.toString()), new TakingTransducer<>(3));
+  public void testMappingTransducer() {
+    Transducer<Integer, String> transducer = new MappingTransducer<>(a -> a.toString());
+    assertEquals("123", transducer.transduce(new CatIteration()).consume(Arrays.asList(1, 2, 3)));
+  }
+  public void testTakingTransducer() {
+    Transducer<Integer, String> transducer = new ComposingTransducer<>(new MappingTransducer<>(a -> a.toString()), new TakingTransducer<>(3));
     assertEquals("123", transducer.transduce(new CatIteration()).consume(Arrays.asList(1, 2, 3, 4)));
+  }
+  public void testComposingTransducerOrder1() {
+    Transducer<String, String> transducer = new ComposingTransducer<>(new TakingTransducer<>(3), new UniquifyingTransducer<>());
+    assertEquals("12", transducer.transduce(new CatIteration()).consume(Arrays.asList("1", "2", "2", "3")));
+  }
+  public void testComposingTransducerOrder2() {
+    Transducer<String, String> transducer = new ComposingTransducer<>(new UniquifyingTransducer<>(), new TakingTransducer<>(3));
+    assertEquals("123", transducer.transduce(new CatIteration()).consume(Arrays.asList("1", "2", "2", "3", "4")));
   }
 }
