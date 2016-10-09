2b105e55189a8448dd9ead55081deecf7e09ac32
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-10-09 16:10:48 +0300

    Filtering and Interspersing transducers

---
+++ src/main/java/com/github/nikita_volkov/java/transducers/FilteringTransducer.java
@@ -0,0 +1,20 @@
+package com.github.nikita_volkov.java.transducers;
+
+import com.github.nikita_volkov.java.iterations.*;
+
+import java.util.function.Predicate;
+
+public final class FilteringTransducer<a> implements Transducer<a, a> {
+
+  private final Predicate<a> predicate;
+
+  public FilteringTransducer(Predicate<a> predicate) {
+    this.predicate = predicate;
+  }
+
+  @Override
+  public <output> Iteration<a, output> transduce(Iteration<a, output> finalIteration) {
+    return new FilterIteration<>(finalIteration, predicate);
+  }
+
+}
---
+++ src/main/java/com/github/nikita_volkov/java/transducers/InterspersingTransducer.java
@@ -0,0 +1,18 @@
+package com.github.nikita_volkov.java.transducers;
+
+import com.github.nikita_volkov.java.iterations.*;
+
+public final class InterspersingTransducer<a> implements Transducer<a, a> {
+
+  private final a separator;
+
+  public InterspersingTransducer(a separator) {
+    this.separator = separator;
+  }
+
+  @Override
+  public <output> Iteration<a, output> transduce(Iteration<a, output> finalIteration) {
+    return new IntersperseIteration<a, output>(finalIteration, separator);
+  }
+
+}
