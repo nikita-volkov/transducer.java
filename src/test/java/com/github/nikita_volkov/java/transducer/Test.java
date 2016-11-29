c664aec1f2ff747cb16d8cf984bd38fc0ef49dda
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-11-29 19:54:21 +0300

    Update to new "reducer"

--- src/test/java/com/github/nikita_volkov/java/transducer/Test.java
+++ src/test/java/com/github/nikita_volkov/java/transducer/Test.java
@@ -1,8 +1,6 @@
 package com.github.nikita_volkov.java.transducer;
 
 import com.github.nikita_volkov.java.reducer.StringCatReducer;
-import com.github.nikita_volkov.java.reducer.reducible.*;
-import com.github.nikita_volkov.java.transducer.transducible.IterableTransducible;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
@@ -10,36 +8,36 @@ import java.util.Arrays;
 public class Test extends TestCase {
   public void testUniquifyingTransducer() {
     Transducer<String, String> transducer = new UniquifyingTransducer<>();
-    assertEquals("123", new IterableReducible<>(Arrays.asList("1", "2", "2", "3")).reduce(transducer.transduce(new StringCatReducer())));
+    assertEquals("123", transducer.transduce(new StringCatReducer()).reduce("1", "2", "2", "3"));
   }
 
   public void testMappingTransducer() {
     Transducer<Integer, String> transducer = new MappingTransducer<>(a -> a.toString());
-    assertEquals("123", new IterableTransducible<>(Arrays.asList(1, 2, 3)).reduce(transducer, new StringCatReducer()));
+    assertEquals("123", transducer.transduce(new StringCatReducer()).reduce(1, 2, 3));
   }
 
   public void testTakingTransducer() {
     Transducer<Integer, String> transducer = new ComposingTransducer<>(new MappingTransducer<>(a -> a.toString()), new TakingTransducer<>(3));
-    assertEquals("123", new ArrayReducible<>(1, 2, 3, 4).reduce(transducer.transduce(new StringCatReducer())));
+    assertEquals("123", transducer.transduce(new StringCatReducer()).reduce(1, 2, 3, 4));
   }
 
   public void testComposingTransducerOrder1() {
     Transducer<String, String> transducer = new ComposingTransducer<>(new TakingTransducer<>(3), new UniquifyingTransducer<>());
-    assertEquals("12", new ArrayReducible<>("1", "2", "2", "3").reduce(transducer.transduce(new StringCatReducer())));
+    assertEquals("12", transducer.transduce(new StringCatReducer()).reduce("1", "2", "2", "3"));
   }
 
   public void testComposingTransducerOrder2() {
     Transducer<String, String> transducer = new ComposingTransducer<>(new UniquifyingTransducer<>(), new TakingTransducer<>(3));
-    assertEquals("123", new ArrayReducible<>("1", "2", "2", "3", "4").reduce(transducer.transduce(new StringCatReducer())));
+    assertEquals("123", transducer.transduce(new StringCatReducer()).reduce("1", "2", "2", "3", "4"));
   }
 
   public void testFlatmappingTransducer() {
     Transducer<Integer, String> transducer = new FlatmappingTransducer<>(i -> i % 2 == 0 ? Arrays.asList(i.toString(), "!") : Arrays.asList());
-    assertEquals("2!4!", new ArrayReducible<>(1, 2, 3, 4, 5).reduce(transducer.transduce(new StringCatReducer())));
+    assertEquals("2!4!", transducer.transduce(new StringCatReducer()).reduce(1, 2, 3, 4, 5));
   }
 
   public void testTransducerBuilder() {
     Transducer<Integer, String> transducer = TransducerBuilder.<Integer>taking(3).unique().map(x -> x.toString());
-    assertEquals("12", new ArrayReducible<>(1, 2, 2, 3).reduce(transducer.transduce(new StringCatReducer())));
+    assertEquals("12", transducer.transduce(new StringCatReducer()).reduce(1, 2, 2, 3));
   }
 }
